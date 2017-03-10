44b499070d389f8aed2c38e6b16019a7390da2fa
Bong <mjbond_bong2@yahoo.com>
2017-03-10 19:43:07 +0800

    add clearance/paperclip/simple_form create index/show page tested ok

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,26 @@
 class RecipesController < ApplicationController
+	before_action :find, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@recipes = Recipe.all
 	end
 
 	def show
+
 	end
 
 	def new
+		@recipe = Recipe.new
 	end
 
 	def create
+		@recipe = Recipe.new(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -20,4 +31,16 @@ class RecipesController < ApplicationController
 
 	def destroy
 	end
+
+	private
+
+	def recipe_params
+	params.require(:recipe).permit(:title, :cook_time, :nutrition, :description, :recipe, :direction)
+	end
+
+	def find
+	@recipe = Recipe.find(params[:id])
+	end
+
+
 end
