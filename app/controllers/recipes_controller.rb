7bb2560a7d2607b4f3420b46317f333c29551f24
Bong <mjbond_bong2@yahoo.com>
2017-03-10 20:19:18 +0800

    create edit/update/destroy pages and also configure clearance and routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
+	before_action :require_login, except: [:show, :index]
 	before_action :find, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@recipes = Recipe.all
+		@recipes = Recipe.all.order(created_at: :desc)
 	end
 
 	def show
@@ -24,12 +25,20 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
+
 	end
 
 	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@recipe.destroy
+		redirect_to root_path
 	end
 
 	private
