682ef62904c9bb79d424ae354802067728e3e1dc
samyinz <yinz84@gmail.com>
2017-03-14 14:52:48 +0800

    commit float to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314054350) do
+ActiveRecord::Schema.define(version: 20170314064841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170314054350) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "tag"
-    t.float    "price"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
