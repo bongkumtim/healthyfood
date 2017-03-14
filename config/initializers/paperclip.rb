34861fa0b919621a59a838c4aaba2f0337badd7b
Bong <mjbond_bong2@yahoo.com>
2017-03-14 16:09:17 +0800

    [WIP] Fixing Paperclip upload

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# Paperclip::Attachment.default_options[:s3_host_name] = 'ap-southeast-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'ap-southeast-1.amazonaws.com'
