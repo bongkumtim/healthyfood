8c62c609c3d00c3653e2e1a15a21193c75e3a45e
samyinz <yinz84@gmail.com>
2017-03-14 17:21:43 +0800

    removed image resizing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
 	belongs_to :user
 	has_many :comments
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "200x200>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   acts_as_votable
 
