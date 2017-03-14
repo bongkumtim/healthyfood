84a8238ccffe34f72936dfa8887a481a60649f55
samyinz <yinz84@gmail.com>
2017-03-14 14:43:19 +0800

    completed front-end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313072021) do
+ActiveRecord::Schema.define(version: 20170314054350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170313072021) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "tag"
+    t.float    "price"
   end
 
   create_table "users", force: :cascade do |t|
